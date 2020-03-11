643a7ce79ac140f4f7981abcb3c12c4091b7f04b
Arsenije Savic <arsenijesavic@me.com>
2020-03-11 15:46:39 +0100

    adding new firebase config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,18 +1,18 @@
-import firebase from "firebase/app"
-import "firebase/analytics"
+import firebase from "firebase/app";
+import "firebase/analytics";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDPlRFVaAbZhzMMGxSWLhX-h50EQZtd-ls",
-  authDomain: "phones-on.firebaseapp.com",
-  databaseURL: "https://phones-on.firebaseio.com",
-  projectId: "phones-on",
-  storageBucket: "phones-on.appspot.com",
-  messagingSenderId: "874335448783",
-  appId: "1:874335448783:web:bc9e616d2d73266ad151ba",
-  measurementId: "G-GGN691ME4Y",
-}
+  apiKey: "AIzaSyBiLhN-ZJT4Gq57CU8LJgiTTkOvufOhjQg",
+  authDomain: "phoneson-art.firebaseapp.com",
+  databaseURL: "https://phoneson-art.firebaseio.com",
+  projectId: "phoneson-art",
+  storageBucket: "phoneson-art.appspot.com",
+  messagingSenderId: "206464800890",
+  appId: "1:206464800890:web:5e458b7066d289dabfc3c3",
+  measurementId: "G-EJMDDY37PT"
+};
 
 if (typeof window !== "undefined" && !firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig)
-  firebase.analytics()
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 }
